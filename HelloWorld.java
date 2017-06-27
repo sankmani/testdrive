018a2d92c63992b956fc05e5d1dd762c3fff9caf
Sankar <sankarmanir@gmail.com>
2017-06-27 14:32:27 -0700

    some init msg

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Adding new line");
+    System.out.println("3rd change");
   }
 }
